1e5bd6d9ece61379ba1b6b9fabbbeeda3359a845
NΞ◎N <90662438+0xNeon-opensource@users.noreply.github.com>
2023-06-24 20:30:54 -0700

    add streaming prototype

--- src/components/TestLangChain.tsx
+++ src/components/TestLangChain.tsx
@@ -22,11 +22,47 @@ const TestLangChain: React.FC = () => {
         test3.mutate();
     }, [test3])
 
+    const testStream = async () => {
+        const res = await fetch("/api/chat", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                query: 'write me a lengthy poem about ai',
+                history: [],
+            }),
+        });
+
+
+        const stream = res.body;
+        console.log(stream)
+
+        const reader = stream!.getReader();
+
+        try {
+            while (true) {
+                const { done, value } = await reader.read();
+                if (done) {
+                    break;
+                }
+
+                const decodedValue = new TextDecoder().decode(value);
+                console.log(decodedValue)
+
+            }
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <div className="flex flex-col gap-4">
             <button onClick={testDocumentLoader}>Test document loader</button>
             <button onClick={testStreamingLlm}>Test Streaming LLM</button>
             <button onClick={testChatMemory}>Test Chat Mem</button>
+            <button onClick={testStream}>Test Stream 🌊💦💧🚣‍♂️</button>
         </div>
     );
 }
